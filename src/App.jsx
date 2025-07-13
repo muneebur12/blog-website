bdbf2f4689a10d5bbe2c57ffbd02ffd16b006df2
Muneeb Ashiq <muneebashiq88@gmail.com>
2025-07-13 15:05:53 +0500

    Add admin dashboard components and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from './Pages/Home'
-import Blog from './Pages/Blog'
+import Blog from './Pages/Blog' 
+import Layout from './Pages/Admin/Layout'
+import Dashboard from './Pages/Admin/Dashboard'
+import ListBlog from './Pages/Admin/ListBlog'
+import AddBlog from './Pages/Admin/AddBlog'
+import Comments from './Pages/Admin/Comments' 
+import Login from './Components/admin/Login'
+import 'quill/dist/quill.snow.css'
 
 function App() {
   return (
@@ -10,6 +17,13 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/blog/:id' element={<Blog />} />
+        <Route path='/admin' element={true ?<Layout />:<Login />}>
+          <Route index element={<Dashboard />} />
+           <Route path='add-blog' element={<AddBlog />} />
+          <Route path='list-blog' element={<ListBlog />} />
+          <Route path='comments' element={<Comments />} />
+
+        </Route>
       </Routes>
       </BrowserRouter>
     </div>
